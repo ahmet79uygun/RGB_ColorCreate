8e2a3a789ff35e12fdf6224a9349cb2a42418c41
ahmet uygun <ahmetuygun140@gmail.com>
2021-09-02 16:02:36 +0300

    Add project files.

---
+++ 201913709036_Ahmet_Uygun/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace _201913709036_Ahmet_Uygun
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.AutoSize = false;
+            this.trackBar3.BackColor = System.Drawing.Color.Blue;
+            this.trackBar3.Location = new System.Drawing.Point(243, 71);
+            this.trackBar3.Maximum = 255;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar3.Size = new System.Drawing.Size(56, 285);
+            this.trackBar3.TabIndex = 5;
+            this.trackBar3.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.trackBar3);
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.trackBar2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(-2, -30);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(312, 359);
+            this.panel1.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(33, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RED";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.BackColor = System.Drawing.Color.Red;
+            this.trackBar1.Location = new System.Drawing.Point(24, 71);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(56, 285);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(243, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "BLUE";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.AutoSize = false;
+            this.trackBar2.BackColor = System.Drawing.Color.Green;
+            this.trackBar2.Location = new System.Drawing.Point(134, 71);
+            this.trackBar2.Maximum = 255;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(56, 285);
+            this.trackBar2.TabIndex = 4;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Emoji", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(132, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "GREEN";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(309, 327);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ 201913709036_Ahmet_Uygun/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _201913709036_Ahmet_Uygun
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// form1 in build edilmesi anında yapılacak işlemler tanımlanır
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Hide();//form başlangıcında panelin gizlenmesini sağlar.
+        }
+
+        /// <summary>
+        /// button un tıklanması durumunda yapılacak işlemler tanımlanır
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Show();//butona basıldığı anda panelin gösterilmesini sağlar bu sayede özellikler kullanılabilir hale gelir
+        }
+
+        /// <summary>
+        /// tarckbar1 in scroll durumunda yapılacak işlemler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            panel1.BackColor = Color.FromArgb(trackBar1.Value, trackBar2.Value, trackBar3.Value); /*trackbar1 scroll hareketinde gelen 
+            değer ve ayrıca diğer trackbarlardan gelen değerleri argb renk kodlamasına atayarak panelin arkaplan rengine eşitliyor*/
+            label1.ForeColor = Color.FromArgb(trackBar1.Value,0,0); //trackbar1 den gelen veriyi argb koduna dönüştürerek label1 in forecolor özelliğine atar
+            this.Text = Convert.ToString(Color.FromArgb(trackBar1.Value, trackBar2.Value, trackBar3.Value));//trackbar1 den gelen veriyi form1 in text kısmına yazmaya yarayan kod. istenilen şekilde olmadı ancak temel anlamda renk kodlarını gösteriyor denilebilir.
+        }
+
+        /// <summary>
+        /// tarckbar2 nin scroll durumunda yapılacak işlemler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            panel1.BackColor = Color.FromArgb(trackBar1.Value, trackBar2.Value, trackBar3.Value);/*trackbar2 scroll hareketinde gelen 
+            değer ve ayrıca diğer trackbarlardan gelen değerleri argb renk kodlamasına atayarak panelin arkaplan rengine eşitliyor*/
+            label2.ForeColor = Color.FromArgb(0,trackBar1.Value,0); //trackbar2 den gelen veriyi argb koduna dönüştürerek label2 nin forecolor özelliğine atar
+            this.Text = Convert.ToString(Color.FromArgb(trackBar1.Value, trackBar2.Value, trackBar3.Value));//trackbar2 den gelen veriyi form1 in text kısmına yazmaya yarayan kod. istenilen şekilde olmadı ancak temel anlamda renk kodlarını gösteriyor denilebilir.
+
+        }
+        /// <summary>
+        /// tarckbar3 ün scroll durumunda yapılacak işlemler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            panel1.BackColor = Color.FromArgb(trackBar1.Value, trackBar2.Value, trackBar3.Value);/*trackbar3 scroll hareketinde gelen 
+            değer ve ayrıca diğer trackbarlardan gelen değerleri argb renk kodlamasına atayarak panelin arkaplan rengine eşitliyor*/
+            label3.ForeColor = Color.FromArgb(0,0,trackBar1.Value); //trackbar3 den gelen veriyi argb koduna dönüştürerek label3 ün forecolor özelliğine atar
+            this.Text = Convert.ToString(Color.FromArgb(trackBar1.Value, trackBar2.Value, trackBar3.Value));//trackbar3 den gelen veriyi form1 in text kısmına yazmaya yarayan kod. istenilen şekilde olmadı ancak temel anlamda renk kodlarını gösteriyor denilebilir.
+        }
+    }
+}
